a9ed0f0548df1afa93ede3181e04711929285dcb
Ann Catherine Jose <ann.jose@gmail.com>
2025-03-15 15:27:38 -0700

    Added a simple Pomodoro class to show a intro message and replaced the default main page with this content

---
+++ src/app/LofiPomodoro.tsx
@@ -0,0 +1,11 @@
+const LofiPomodoro = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-2xl">Lofi Pomodoro</h1>
+            <p>Work for 25 minutes, then take a 5 minute break.</p>
+            <p>Repeat this cycle 4 times, then take a 15 minute break.</p>
+        </div>
+    );
+};
+
+export default LofiPomodoro;
